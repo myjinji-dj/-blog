985801c1597947a5e7dfe8ae93ae8b4b2b275150
Myjinji-my <7350513+myjinjimy@user.noreply.gitee.com>
2020-05-04 23:10:37 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '好想睡觉啊', // html页面的title 
+  description: '此时想法',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: 'auto'
+  }
+}
